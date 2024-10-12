e471ce802f4e2f2da7dca0dee2944f6d2228e42d
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-10-12 11:13:39 +0600

    hooks and middleware done

---
+++ src/hooks/auth.ts
@@ -0,0 +1,29 @@
+
+import { loginUser, registerUser } from "@/services/auth-services";
+import { useMutation } from "@tanstack/react-query";
+import { FieldValues } from "react-hook-form";
+import { toast } from "sonner";
+export const useUserRegistration = () => {
+  return useMutation<any, Error, FieldValues>({
+    mutationKey: ["USER_REGISTRATION"],
+    mutationFn: async (userData) => await registerUser(userData),
+    onSuccess: () => {
+      toast.success("User registration successful.");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+};
+export const useUserLogin = () => {
+  return useMutation<any, Error, FieldValues>({
+    mutationKey: ["USER_LOGIN"],
+    mutationFn: async (userData) => await loginUser(userData),
+    onSuccess: () => {
+      toast.success("User login successful.");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+};
---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getCurrentUser } from "./services/auth-services";
+type Role = keyof typeof roleBasedPrivateRoutes;
+const authRoutes = ["/login", "/register"];
+const roleBasedPrivateRoutes = {
+  user: [/^\/dashboard\/user/],
+  admin: [/^\/dashboard\/admin/],
+};
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const user = await getCurrentUser();
+  if (!user) {
+    if (authRoutes.includes(pathname)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(
+        new URL(`/login?redirect=${pathname}`, request.url)
+      );
+    }
+  }
+  if (user.role && roleBasedPrivateRoutes[user.role as Role]) {
+    const routes = roleBasedPrivateRoutes[user?.role as Role];
+    if (routes.some((route) => pathname.match(route))) {
+      return NextResponse.next();
+    }
+  }
+  return NextResponse.redirect(new URL("/", request.url));
+}
+export const config = {
+  matcher: ["/login", "/register", "/dashboard/:page*"],
+}
