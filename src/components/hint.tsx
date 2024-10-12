352c203618a526a0c02472ce3b92e6fa2b2b6c68
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-10-12 18:24:07 +0600

    added emoji popover and hint and modified editor

---
+++ src/components/hint.tsx
@@ -0,0 +1,35 @@
+'use client'
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
+
+
+  interface HintProps {
+    children: React.ReactNode;
+    label: string;
+    side?: "top" | "right" | "bottom" | "left";
+    align?: "start" | "center" | "end"; 
+  }
+
+  export const Hint = ({
+    children,
+    label,
+    side,
+    align
+  }: HintProps) => {
+    return (
+        <TooltipProvider>
+            <Tooltip delayDuration={50}>
+                <TooltipTrigger asChild>
+                    {children}
+                </TooltipTrigger>
+                <TooltipContent side={side} align={align} className="bg-black text-white border border-white/5">
+                    <p className="font-medium text-xs">{label}</p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    )
+  }
